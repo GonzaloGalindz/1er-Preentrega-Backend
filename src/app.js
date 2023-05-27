876894394b81442440d84797cd8afb6352508244
GonzaloGalindz <gonzagalin777@gmail.com>
2023-05-27 00:00:29 -0300

    Primer-preentrega

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const port = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+
 app.use("/api/products", routerProducts);
 app.use("/api/carts", routerCarts);
 
--- src/utils.js
+++ src/utils.js
@@ -201,7 +201,7 @@ export class CartManager {
       this.carts = [...this.carts, newCart];
       if (this.carts) {
         const cartsToJSON = JSON.stringify(this.carts);
-        await fs.promises.writeFile("../carts.json", cartsToJSON);
+        await fs.promises.writeFile("./carts.json", cartsToJSON);
         msg.data.push(newCart);
       }
     } catch (error) {
@@ -264,7 +264,7 @@ export class CartManager {
           this.carts = newCartList;
           //actualizo el JSON de carritos
           const cartsToJSON = JSON.stringify(this.carts);
-          await fs.promises.writeFile("../carts.json", cartsToJSON);
+          await fs.promises.writeFile("./carts.json", cartsToJSON);
           //para evitar duplicados o inconvenientes, se crea el array vacio
           msg.data = [];
           //retorno el carrito para mostrarlo al cliente
@@ -285,7 +285,7 @@ export class CartManager {
           //actualizo la lista de carritos
           this.carts = newCartList;
           const cartsToJSON = JSON.stringify(this.carts);
-          await fs.promises.writeFile("../carts.json", cartsToJSON);
+          await fs.promises.writeFile("./carts.json", cartsToJSON);
           //para evitar duplicados o inconvenientes, se crea el array vacio
           msg.data = [];
           //se pushea la informacion
